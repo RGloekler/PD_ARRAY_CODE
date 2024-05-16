8940ffe6019ee2c74e6ff7f2937394e12307304b
RGloekler <47490659+RGloekler@users.noreply.github.com>
2024-05-16 13:13:08 -0700

    Add files via upload

---
+++ live_BGSUB.py
@@ -0,0 +1,195 @@
+# Ryan Gloekler, Hunt Vacuum Microelectronics Lab, MGXI Project
+# @author: regloekler@ucdavis.edu
+# last updated 2/29/2024
+
+# Operation: Run draw_pixels_updated.py from the terminal. This will create a data
+# file containing background signal, for the first 150 samples. The program will then
+# terminate. Subsequent runs will not terminate, unless by the user, and will instead
+# plot the live data, so that changes at the photodiode interface can be seen in
+# real time. The values displayed have the average of the background data subtacted from
+# them, so as to ensure that as much noise can be cut from the sensor readings as possible.
+
+# This file has been replaced..  should be used for live monitoring of the PD_array,
+# rather than data collection. Data collection should be handled with read_pixels_fast.py
+
+# NOTE: For this code to work, simple firmware writing the data to serial is needed. This code is
+# now deprecated.
+
+from datetime import *
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import draw
+import numpy as np
+import serial, re, sys, csv, os, time
+
+MULTIPLIER = 10 # a value that multiplies all of the observed pixel values by 10, so that they more easily show up on live updating script
+
+bg_data = 'background_pixelData.csv' # used to create csv files...
+data_csv = 'bg_subtracted_data.csv'  # used to create new data files (bg_sub)
+
+# grab current data from the serial bus
+def grab_serial(ser):
+    line = ser.readline()
+    #line = sys.stdout.write(str(ser.readline()))
+    sys.stdout.flush()
+    return str(line)
+
+# used for scaling a matrix of values - not used for averaged pixel value cases
+def scale_values(data_array):
+    # normalize
+    for val in range(len(data_array)):
+        try: data_array[val] /= 1023
+        except: print("Couldn't convert... Trying again")
+    return data_array
+
+# create a new array that can be plotted (expected data format)
+def make_plottable(input_data):
+    plottable_data = []
+    for val in range(0,2): plottable_data.append(input_data)
+    return plottable_data
+
+# creates a new csv file that background data can be written to
+def create_csv(filenm):
+    # create a csv file to write background noise data to, and a header array
+    file = open('./' + filenm, 'w', encoding = 'UTF8', newline='')
+    header = ['Pixel 1', 'Pixel 2', 'Pixel 3', 'Pixel 4', 'Pixel 5', 'Pixel 6', 'Pixel 7',
+    'Pixel 8', 'Pixel 9', 'Pixel 10', 'Pixel 11', 'Pixel 12', 'Pixel 13', 'Pixel 14', 'Pixel 15', 'Pixel 16', 'TIME']
+
+    writer = csv.writer(file)
+    writer.writerow(header)
+    return writer
+
+# get the averaged data from the existing data file
+def collect_csv_data():
+    # read from csv file, and get the average of each column for
+    # background subtraction
+
+    data_array = []
+    with open(bg_data, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for i, line in enumerate(reader):
+            data_array.append(line)
+        data_array.pop(0)
+
+    averaged = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for row in range(len(data_array)):
+        data_row = data_array[row][0].split(',')
+        for val in range(len(data_row)):
+            averaged[val] += float(data_row[val])
+
+    averaged = [element / len(data_array) for element in averaged]
+    return averaged
+
+# main loop: creates figure, sets up serial communication, gets data from serial,
+# performs background subtraction, and live plots the PD array data
+def main():
+    averaged_time = []
+    timer_counter = 0
+    # establish comms with the arduino
+    ser = serial.Serial(port='COM3', baudrate=9600, timeout = 1)
+
+    # setup regex, and grab some serial data
+    non_decimal = re.compile(r'[^\d,]+')
+
+    placeholder = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.0, 0.8, 0.5, 0.2]
+
+    # Set the figure size, and scale
+    plt.rcParams["figure.figsize"] = [7, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+
+    print('\nPlotting light-intensity map')
+
+    # set up the figure for plotting pixels
+    fig, ax = plt.subplots(1,1)
+    image = make_plottable(placeholder)
+    im = ax.imshow(image, cmap='Reds', vmin = 0, vmax= 1)
+    ax.set_title('PD Array Output')
+    ax.set_xlabel('Pixel Number')
+
+    # initiate the main loop of the program... Grab and plot data
+    # log the first several hundred samples to a csv file for background subtraction
+
+    # artifact of older code that used background subtaction from excel file
+    background_counter = -1
+    writer = None
+    averaged = collect_csv_data()
+    data_writer = create_csv(data_csv)
+
+    # create a variable to store the maximum observed pixel value, over many cycles 
+    CURRENT_MAX = 0
+    while True:
+        
+        initial_time = time.perf_counter()
+        # grab a new set of data from each of the pixels
+        new_data = grab_serial(ser).split(',')
+
+        # convert all values to integers, so they may be scaled
+        for val in range(len(new_data)):
+            converted = non_decimal.sub('', new_data[val])
+            if converted: new_data[val] = int(converted)
+
+        # scale the values for plotting, and get an array from them that can be
+        # plotted
+        scaled = scale_values(new_data)
+        #print(type(scaled[0]))
+
+        
+        # optionally include background subtraction (with noise data from excel)
+        # bg_vals = [0.002531769,0.074379277,0.074613881,0.026774194,0.002326491,0,0.000645161,0.048807429,0.084623656,0.109032258,0.010420332,0.00516129,0.099921799,0.00285435,0.105913978,0.08113392]
+        bg_vals = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        scaled = [val - bg_vals[scaled.index(val)] for val in scaled]
+        
+        
+        plottable = [[]]
+
+        """ SKIPPING BG SUBTRACTION; negligible dark current, not needed for live updating purposes.
+        if bg_data in os.listdir('./'): # dont bg sub if there is no bg
+            for val in range(len(scaled)):
+                try:
+                    scaled[val] = float(scaled[val]) - float(averaged[val]) # perform the background subtraction, and plot
+                except:
+                    print("Couldn't update.. Trying again")
+        """
+        #print(scaled, end='')
+        for val in scaled: print('%.7f' % (float(val) * MULTIPLIER), end = ', ')
+        print(',  MAX VALUE: ' + str('%.7f' % max(scaled)) + ' Pixel: ' + str(scaled.index(max(scaled))), end = ' ')
+
+        if max(scaled) > CURRENT_MAX:
+            CURRENT_MAX = max(scaled) * MULTIPLIER
+        print('Overall MAX: ' + str('%.7f' % CURRENT_MAX))
+        
+        
+        
+        plottable = make_plottable(scaled)
+        plottable = [[val*MULTIPLIER for val in row] for row in plottable]
+                
+            
+        #print(plottable)
+        try: # update the canvas...
+            image = plottable
+            im.set_data(image)
+            fig.canvas.draw_idle()
+            plt.pause(0.00001)
+        except:
+            print('Couldnt update canvas... Data error.')
+
+
+        # write the scaled data to the appropriate spreadsheet
+        if writer: writer.writerow(scaled)
+        else:
+            scaled.append(str(datetime.now())) # add the time to the data...
+            data_writer.writerow(scaled)
+
+        # check conditionals for background data
+        if background_counter > -1: background_counter += 1
+        if background_counter >= 150: exit('Finished Collecting Background data.')
+        final_time = time.perf_counter()
+
+        timer_counter += 1
+        averaged_time.append(final_time - initial_time)
+        if timer_counter % 200 == 0:
+            print(np.mean(averaged_time))
+    file.close()
+
+if __name__ == '__main__':
+    main()
---
+++ single_shot_BGSUB.py
@@ -0,0 +1,170 @@
+# @author Ryan Gloekler, UC Davis. Hunt Vacuum Microectronics Lab
+# regloekler@ucdavis.edu
+# Reads and processes Serial data recieved from Arduino ADC module, sampling at 10KHz for 10ms.
+# Plots the average data across the sensor over this time period.
+# Stores data samples for each shot in 'data_collection_dump.csv' for later analysis
+# Last updated: 2/28/2024
+
+from datetime import *
+from time import *
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import draw
+import serial, re, sys, csv, os
+import numpy as np
+
+MULTIPLIER = 10
+#------------------------ DEFINE HELPER FUNCTIONS ------------------------------
+
+# grab current data from the serial bus
+def grab_serial(ser):
+    line = ser.readline()
+    #line = sys.stdout.write(str(ser.readline()))
+    sys.stdout.flush()
+    return str(line)
+
+# used for scaling a matrix of values - not used for averaged pixel value cases
+def scale_values(data_array):
+    # normalize
+    for val in range(len(data_array)):
+        try: data_array[val] /= 1023 # arduino uses a 10-bit ADC, 1024 values
+        except: print("Couldn't convert... Trying again")
+    return data_array
+
+# create a new array that can be plotted (expected data format)
+def make_plottable(input_data):
+    plottable_data = []
+    for val in range(0,2): plottable_data.append(input_data)
+    return plottable_data
+
+# creates a new csv file that background data can be written to
+def create_csv(filenm):
+    # create a csv file to write background noise data to, and a header array
+    file = open('./' + filenm, 'a', encoding = 'UTF8', newline='')
+    header = ['Pixel 1', 'Pixel 2', 'Pixel 3', 'Pixel 4', 'Pixel 5', 'Pixel 6', 'Pixel 7',
+    'Pixel 8', 'Pixel 9', 'Pixel 10', 'Pixel 11', 'Pixel 12', 'Pixel 13', 'Pixel 14', 'Pixel 15', 'Pixel 16', 'Sample No.', 'Serial Time']
+
+    writer = csv.writer(file)
+    writer.writerow(header)
+    return writer
+
+#----------------------- IMPLEMENT MAIN FUNCTIONALITY --------------------------
+# since the heavy lifting is done in firmware, we just need to read all the
+# data from serial here...
+def main():
+    # search for csv, create one if it is not found
+    filenm = 'shot_data/data_collection_dump.csv'
+    if filenm not in os.listdir('./'):
+        file_writer = create_csv(filenm)
+    else:
+        file_writer = csv.writer(open(filenm, 'a', encoding = 'UTF8', newline=''))
+
+    # set up serial communication
+    ser = serial.Serial(port='COM3', baudrate=115200, timeout = 1)
+    # setup regex for data parsing
+    non_decimal = re.compile(r'[^\d,]+')
+
+    # set sample counter to 0
+    counter = 0
+
+    # main loop of operation - constanty scan serial,
+    # until we recieve data from ADC (firmware has been triggered)
+    averaged = []
+    averaged_np = []
+    new_shot = True
+    while True:
+        line = grab_serial(ser).split(', ')
+        if len(line) > 2: # get only proper pixel data from serial
+            recieved_time = perf_counter()
+            # if a shot has occurred, create a new CSV file for it.
+            if new_shot:
+                cur_time = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+                print(cur_time)
+                new_shot = False # set the new shot flag, so we dont re-create files for each line
+            
+                newfile = 'shot_data/shot_' + cur_time +'.csv'
+                if newfile not in os.listdir('./'):
+                    file_writer = create_csv(newfile)
+                else:
+                    file_writer = csv.writer(open(newfile, 'a', encoding = 'UTF8', newline=''))
+
+
+            
+            # create processing array and start counting data points
+            processed = []
+            counter += 1
+
+            # convert all values to integers, omit all other characters
+            for val in range(len(line)):
+                converted = non_decimal.sub('', line[val])
+                processed.append(converted)
+
+            # trim array to include only floating point values
+            processed = [int(val) for val in processed]
+
+            # scale values to range of 0-1, omitting the sample number row
+            scaled = scale_values(processed[0:-1])
+            
+            # optionally include background subtraction (with noise data from excel)
+            #bg_vals = [0.002600196,0.076774194,0.077556207,0.028670577,0.002424242,0,0.000371457,0.051104594,0.085278592,0.110215054,0.011612903,0.005679374,0.09914956,0.0028348,0.105493646,0.082091887]
+            bg_vals = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            scaled = [val - bg_vals[scaled.index(val)] for val in scaled]
+            
+            # convert data to floating point and get a plottable version for later
+            scaled_converted = [float(val) for val in scaled]
+            averaged.append(scaled_converted)
+
+            plottable = make_plottable(scaled_converted)
+            
+            
+            
+            # add metadata to the data, for final excel readability
+            scaled.append(processed[-1])
+            scaled.append(str(datetime.now()))
+            # print(scaled)
+
+            # write the current set of pixel data to csv, and close file
+            file_writer.writerow(scaled)
+            
+            # print out a scaled version of the data to terminal, much like is done in live-update script
+            printable = [val * MULTIPLIER for val in scaled[0:-2]]
+            print(printable)
+
+            
+            # if recieved a complete set of data, plot it
+            if counter % 100 == 0:
+                final_time = perf_counter()
+                print('Final data transfer, processing, and csv write time is: ' + str(final_time - recieved_time))
+                # if we are ready to finish handling this shot, average data
+                # for plotting
+
+                # print(averaged)
+
+                # create a numpy array object to handle averaging
+                # uncomment this block to enable averaging over the total 10ms
+                # period
+                print('\nPLOTTING DATA AVERAGED OVER 10ms')
+                np_array = np.array(averaged)
+                averaged_np = np.mean(np_array, axis=0)
+                plottable = make_plottable(averaged_np)
+                plottable = [[val*MULTIPLIER for val in row] for row in plottable]
+
+                # Set the figure size, and scale
+                plt.rcParams["figure.figsize"] = [7, 3.50]
+                plt.rcParams["figure.autolayout"] = True
+
+                print('\nPlotting light-intensity map...')
+
+                # set up the figure for plotting pixels
+                fig, ax = plt.subplots(1,1)
+                image = plottable
+                im = ax.imshow(image, cmap='Reds', vmin = 0, vmax= 1)
+                ax.set_title('PD Array Output')
+                ax.set_xlabel('Pixel Number')
+                plt.show() # plot the last sample recieved
+                averaged = [] # reset the averaging list for the next shot
+                
+                new_shot = True # reset shot flag for next shot data collection
+                
+
+if __name__ == '__main__':
+    main()
